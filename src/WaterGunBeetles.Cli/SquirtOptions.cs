c15e72a4c3a48d40d291485ea2487c080c537627
Sebastien Lambla <seb@serialseb.com>
2018-04-17 08:04:22 +0200

    Provide more helpful messages

--- src/WaterGunBeetles.Cli/SquirtOptions.cs
+++ src/WaterGunBeetles.Cli/SquirtOptions.cs
@@ -6,35 +6,35 @@ namespace WaterGunBeetles.Cli
   [Verb("squirt", HelpText = "Start a load test.")]
   class SquirtOptions
   {
-    [Option]
-    public string PackagePath { get; set; }
-
-    [Option('m', "memory", Default = 128, HelpText = "The memory size used for the lambda function")]
-    public int MemorySize { get; set; }
-
     [Option('r', "rate", Required = true, HelpText = "Number of requests/s to achieve")]
     public int RequestsPerSecond { get; set; }
 
     [Option('d', "duration", Required = true, HelpText = "Length of time to run the load test for, for example '1m' or '1h'.")]
     public string Duration { get; set; }
     
+    [Option('w', "warmup", Default = null, HelpText = "Duration of the warm-up.")]
+    public string WarmUpTime { get; set; }
+
     [Option('n', "name", Required = false, HelpText = "Name of the Configuration object in the project. Defaults to the first one found.")]
     public string Name { get; set; }
     
-    [Option('w', "warmup", Default = null, HelpText = "Duration of the warm-up.")]
-    public string WarmUpTime { get; set; }
+    [Option('m', "memory", Default = 128, HelpText = "The memory size used for the lambda function")]
+    public int MemorySize { get; set; }
 
     [Option('v', "verbose", Default = false, HelpText="Output verbose information")]
     public bool Verbose { get; set; }
 
-    [Option("configuration", Default = "Release")]
+    [Option("configuration", Default = "Release", HelpText="MSBuild configuration for builds and lambda packaging")]
     public string Configuration { get; set; }
     
-    [Option("framework", Default="netcoreapp2.0")]
+    [Option("framework", Default="netcoreapp2.0", HelpText="MSBuild framework for builds and lambda packaging")]
     public string Framework { get; set; }
     
-    [Option("rebuild", Default = false)]
+    [Option("rebuild", Default = false, HelpText="Rebuild the current project before runnign the load test.")]
     public bool Rebuild { get; set; }
+    
+    [Option(HelpText = "Path to a lambda package to upload")]
+    public string PackagePath { get; set; }
 
     public void Validate()
     {
